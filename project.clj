c908bc0b9fb1805f95d67b4ecd5d5d9b7c2d07a7
Aaron Steele <eightysteele@gmail.com>
2012-07-04 08:54:36 -0700

    Add cartodb-java-client dep and cleanup query API.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cartodb-clj "1.0.0-SNAPSHOT"
-  :description "Access CartoDB data from Clojure."
+(defproject cartodb-clj "1.1.1-SNAPSHOT"
+  :description "CartoDB client."
   :source-path "src/clj"
   :resources-path "resources"
   :dev-resources-path "dev"
@@ -8,12 +8,13 @@
              "-XX:+UseConcMarkSweepGC"
              "-Xms1024M" "-Xmx1048M" "-server"]
   :javac-options {:debug "true" :fork "true"}
-  :plugins [[swank-clojure "1.4.0-SNAPSHOT"]
+  :plugins [[lein-swank "1.4.4"]
             [lein-clojars "0.9.0"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clojure-csv/clojure-csv "2.0.0-alpha1"]
                  [clj-http "0.4.3"]
                  [cheshire "4.0.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/data.json "0.1.2"]]
+                 [org.clojure/data.json "0.1.2"]
+                 [cartodb-java-client "1.0.0"]]
 :dev-dependencies [[midje "1.4.0"]])
