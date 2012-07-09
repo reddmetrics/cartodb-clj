d7f39b8d79111eaeec32612399588890ae316acf
Dan Hammer <dan.s.hammer@gmail.com>
2012-07-08 17:20:19 -0700

    added namespace comments to playground.clj

--- src/clj/cartodb/playground.clj
+++ src/clj/cartodb/playground.clj
@@ -1,17 +1,26 @@
 (ns cartodb.playground
+  "This namespace gives examples of how to use the API to compose
+application-specific queries to CartoDB.  These are not tests because
+we do not include our private credentials in the public repo.  Once
+`creds` are defined, as below, the commands will clear a pre-existing
+CartoDB table and then add rows to it.
+
+(def creds {:key \"CARTODB_OAUTH_KEY\"
+            :secret \"CARTODB_OAUTH_SECRET\"
+            :user \"CARTODB_USER\"
+            :password \"CARTODB_PASSWORD\"})
+
+NOTE: Before you can run these commands, you'll need the following
+in place:
+
+(1) A local variable `creds` defined as in the README.
+(2) A private CartoDB table with at least two columns, with names
+`x` (a numeric variable) and `y` (string).  
+
+The following queries will clear the CartoDB table, and then insert
+two rows."
   (:use [cartodb.core]
         [cartodb.utils]))
-
-;; NOTE: Before you can run these commands, you'll need the following
-;; in place:
-
-;; (1) A local variable `creds` defined as in the README.
-;; (2) A private CartoDB table with at least two columns, with names
-;; "x" (a numeric variable) and "y" (string).  
-
-;; The following queries will clear the CartoDB table, and then insert
-;; two rows.  
-
 
 (defn delete-all
   "Delete all rows from the specified table.  CAREFUL with this.
